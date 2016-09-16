e121d4d248e41f8149443a7a07807865af37e78c
cynter <cynteran27@gmail.com>
2016-09-16 16:22:30 -0300

    Ejercicio02 - Descuento

---
+++ Ejercicio02/Ejercicio02/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Ejercicio02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grpInformacion = new System.Windows.Forms.GroupBox();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.txtDescuento = new System.Windows.Forms.TextBox();
+            this.lbltotal = new System.Windows.Forms.Label();
+            this.lblDescuento = new System.Windows.Forms.Label();
+            this.lblIngreso = new System.Windows.Forms.Label();
+            this.txtIngreso = new System.Windows.Forms.TextBox();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.grpInformacion.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpInformacion
+            // 
+            this.grpInformacion.Controls.Add(this.txtTotal);
+            this.grpInformacion.Controls.Add(this.txtDescuento);
+            this.grpInformacion.Controls.Add(this.lbltotal);
+            this.grpInformacion.Controls.Add(this.lblDescuento);
+            this.grpInformacion.Controls.Add(this.lblIngreso);
+            this.grpInformacion.Controls.Add(this.txtIngreso);
+            this.grpInformacion.ForeColor = System.Drawing.Color.SteelBlue;
+            this.grpInformacion.Location = new System.Drawing.Point(12, 12);
+            this.grpInformacion.Name = "grpInformacion";
+            this.grpInformacion.Size = new System.Drawing.Size(260, 157);
+            this.grpInformacion.TabIndex = 0;
+            this.grpInformacion.TabStop = false;
+            this.grpInformacion.Text = "Información";
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Enabled = false;
+            this.txtTotal.Location = new System.Drawing.Point(185, 109);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(56, 20);
+            this.txtTotal.TabIndex = 0;
+            // 
+            // txtDescuento
+            // 
+            this.txtDescuento.Enabled = false;
+            this.txtDescuento.Location = new System.Drawing.Point(185, 74);
+            this.txtDescuento.Name = "txtDescuento";
+            this.txtDescuento.Size = new System.Drawing.Size(56, 20);
+            this.txtDescuento.TabIndex = 0;
+            // 
+            // lbltotal
+            // 
+            this.lbltotal.AutoSize = true;
+            this.lbltotal.Enabled = false;
+            this.lbltotal.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbltotal.Location = new System.Drawing.Point(103, 112);
+            this.lbltotal.Name = "lbltotal";
+            this.lbltotal.Size = new System.Drawing.Size(76, 13);
+            this.lbltotal.TabIndex = 0;
+            this.lbltotal.Text = "Total a pagar: ";
+            // 
+            // lblDescuento
+            // 
+            this.lblDescuento.AutoSize = true;
+            this.lblDescuento.Enabled = false;
+            this.lblDescuento.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblDescuento.Location = new System.Drawing.Point(74, 77);
+            this.lblDescuento.Name = "lblDescuento";
+            this.lblDescuento.Size = new System.Drawing.Size(105, 13);
+            this.lblDescuento.TabIndex = 0;
+            this.lblDescuento.Text = "Descuento recibido: ";
+            // 
+            // lblIngreso
+            // 
+            this.lblIngreso.AutoSize = true;
+            this.lblIngreso.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblIngreso.Location = new System.Drawing.Point(15, 22);
+            this.lblIngreso.Name = "lblIngreso";
+            this.lblIngreso.Size = new System.Drawing.Size(133, 13);
+            this.lblIngreso.TabIndex = 0;
+            this.lblIngreso.Text = "Ingrese el monto a cobrar: ";
+            // 
+            // txtIngreso
+            // 
+            this.txtIngreso.Location = new System.Drawing.Point(154, 19);
+            this.txtIngreso.Name = "txtIngreso";
+            this.txtIngreso.Size = new System.Drawing.Size(67, 20);
+            this.txtIngreso.TabIndex = 1;
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(12, 210);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(75, 23);
+            this.btnAceptar.TabIndex = 1;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(107, 210);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 2;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(197, 210);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 3;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.grpInformacion);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.grpInformacion.ResumeLayout(false);
+            this.grpInformacion.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpInformacion;
+        private System.Windows.Forms.Label lblIngreso;
+        private System.Windows.Forms.TextBox txtIngreso;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.TextBox txtDescuento;
+        private System.Windows.Forms.Label lbltotal;
+        private System.Windows.Forms.Label lblDescuento;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
---
+++ Ejercicio02/Ejercicio02/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            
+                double total = double.Parse(txtIngreso.Text);
+                double descuento = 0;
+                double dsc = 0;
+                if (total >= 30 && total <= 50)
+                {
+                    descuento = 0.10;
+                    dsc = total * 0.10;
+                    txtDescuento.Text = dsc.ToString();
+
+                }
+                else if (total > 50)
+                {
+                    descuento = 0.20;
+                    dsc = total * 0.20;
+                    txtDescuento.Text = dsc.ToString();
+                }
+                else
+                {
+                    descuento = 0;
+                    dsc = total * 0;
+                    txtDescuento.Text = dsc.ToString();
+                }
+                txtTotal.Text = (total * (1 - descuento)).ToString();
+                     
+            
+            
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+
+            //foreach (Control control in txtIngreso.Controls)
+            //{
+            //    if (control is TextBox)
+            //    {
+            //        ((TextBox)control).Clear();
+            //    }
+            //}
+
+            this.txtIngreso.Clear();
+
+        }
+
+
+    }
+}
